7a57abc38390fbc7b3b1cea31001b0e82c901693
Ahmed <ahmedmohameddx80@gmail.com>
2024-05-12 07:06:32 +0300

    start app

---
+++ lib/e_commerce_app.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class Ecommerce extends StatelessWidget {
+  const Ecommerce({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'E-Commerce',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: Scaffold(
+          appBar: AppBar(
+            title: const Text(
+              'E commers',
+            ),
+          ),
+        ));
+  }
+}
