6fb3bb6918993edec85fce29248add40a3d9d3f8
Ahmed <ahmedmohameddx80@gmail.com>
2024-05-16 08:34:31 +0300

    font family with its style done

--- lib/e_commerce_app.dart
+++ lib/e_commerce_app.dart
@@ -1,4 +1,6 @@
 import 'package:e_commerce_app/core/utils/connectivity_controller.dart';
+import 'package:e_commerce_app/core/utils/font_helper.dart';
+import 'package:e_commerce_app/core/utils/font_wieght_styles.dart';
 import 'package:e_commerce_app/core/widgets/no_connection_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -29,7 +31,28 @@ class Ecommerce extends StatelessWidget {
                 appBar: AppBar(
                   title: const Text(
                     'E commers',
+                    style: TextStyle(fontFamily: FontFamilyName.arabicFont),
                   ),
+                ),
+                body: Center(
+                  child: Column(children: [
+                    Text(
+                      'English',
+                      style: TextStyle(
+                        fontFamily: FontFamilyName.englishFont,
+                        fontWeight: FontWieghtStyle.bold,
+                        fontSize: 40,
+                      ),
+                    ),
+                    Text(
+                      'عربي',
+                      style: TextStyle(
+                        fontFamily: FontFamilyName.arabicFont,
+                        fontWeight: FontWieghtStyle.bold,
+                        fontSize: 40,
+                      ),
+                    ),
+                  ]),
                 ),
               ),
             ),
