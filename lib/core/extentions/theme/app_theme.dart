6893bffd6f70f4d12d5a7983a9ef46c53843afdd
Ahmed <ahmedmohameddx80@gmail.com>
2024-05-16 20:56:15 +0300

    theme done ✅

---
+++ lib/core/extentions/theme/app_theme.dart
@@ -0,0 +1,19 @@
+import 'package:e_commerce_app/core/styles/colors/dark_colors.dart';
+import 'package:e_commerce_app/core/styles/colors/light_colors.dart';
+import 'package:flutter/material.dart';
+
+import 'colors_extention.dart';
+
+ThemeData darkTheme() {
+  return ThemeData(
+    scaffoldBackgroundColor: DarkColors.mainColor,
+    extensions: <ThemeExtension<MyColors>>[MyColors.dark]
+  );
+}
+
+ThemeData lightThem() {
+  return ThemeData(
+    scaffoldBackgroundColor: LightColors.mainColor,
+    extensions: <ThemeExtension<MyColors>>[MyColors.light]
+  );
+}
