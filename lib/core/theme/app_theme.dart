88795d968a794ee84ec23f8ff2713524103fc0ea
Ahmed <ahmedmohameddx80@gmail.com>
2024-05-18 09:22:04 +0300

    add custom widgets

--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -1,5 +1,6 @@
 import 'package:e_commerce_app/core/styles/colors/dark_colors.dart';
 import 'package:e_commerce_app/core/styles/colors/light_colors.dart';
+import 'package:e_commerce_app/core/utils/font_family_center.dart';
 import 'package:flutter/material.dart';
 
 import 'colors_extension.dart';
@@ -7,13 +8,25 @@ import 'colors_extension.dart';
 ThemeData darkTheme() {
   return ThemeData(
     scaffoldBackgroundColor: DarkColors.mainColor,
-    extensions: <ThemeExtension<MyColors>>[MyColors.dark]
+    extensions: <ThemeExtension<MyColors>>[MyColors.dark],
+    textTheme: TextTheme(
+      displaySmall:TextStyle(
+        color: DarkColors.white,
+        fontFamily: FontFamilyName.getLocalizedFontFamily(),
+      ) 
+    )
   );
 }
 
 ThemeData lightThem() {
   return ThemeData(
     scaffoldBackgroundColor: LightColors.mainColor,
-    extensions: <ThemeExtension<MyColors>>[MyColors.light]
+    extensions: <ThemeExtension<MyColors>>[MyColors.light],
+      textTheme: TextTheme(
+      displaySmall:TextStyle(
+        color: LightColors.black,
+        fontFamily: FontFamilyName.getLocalizedFontFamily(),
+      ) 
+    )
   );
 }
--- lib/core/utils/font_helper.dart
+++
@@ -1,6 +0,0 @@
-class FontFamilyName {
-  static const String arabicFont = 'Cairo';
-  static const String englishFont = 'Poppins';
-
-  // TODO switch between arabic and english Fonts
-}
