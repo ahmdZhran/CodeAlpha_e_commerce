59758acea42d4ea3d9f284ddf0eaf977356b2410
Ahmed <ahmedmohameddx80@gmail.com>
2024-05-22 00:06:24 +0300

    create login method

---
+++ lib/features/auth/data/data_source/auth_data_source.dart
@@ -0,0 +1,17 @@
+import 'package:e_commerce_app/core/services/graphql/api_services.dart';
+import 'package:e_commerce_app/core/services/graphql/graphql_queries/auth/auth_queries.dart';
+import 'package:e_commerce_app/features/auth/data/models/login_request_body.dart';
+import 'package:e_commerce_app/features/auth/data/models/login_response.dart';
+
+class AuthDataSource {
+  final ApiServices _graphql;
+
+  AuthDataSource(this._graphql);
+ 
+ //Login 
+  Future<LoginRespose> login({required LoginRequestBody body}) async {
+    final response =
+        await _graphql.login(AuthQueries().loginMapQuery(body: body));
+    return response;
+  }
+}
