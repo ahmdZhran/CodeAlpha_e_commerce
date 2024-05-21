4db09fa268aaca7499b6e1ff42e98579a8275629
Ahmed <ahmedmohameddx80@gmail.com>
2024-05-21 15:39:28 +0300

    add ApiServices to dependcey injection

--- lib/core/di/dependency_injector.dart
+++ lib/core/di/dependency_injector.dart
@@ -1,12 +1,16 @@
 import 'package:e_commerce_app/core/app/app_cubit/cubit/app_cubit.dart';
+import 'package:e_commerce_app/core/graphql/api_services.dart';
+import 'package:e_commerce_app/core/graphql/dio_factory.dart';
 import 'package:get_it/get_it.dart';
 
 final getIt = GetIt.instance;
 
 Future<void> setupServiceLocator() async {
- await _inItCore();
+  await _inItCore();
 }
- 
-Future <void> _inItCore()async {
-   getIt..registerFactory(AppCubit.new);
+
+Future<void> _inItCore() async {
+  final dio = DioFactory.getDio();
+  getIt..registerFactory(AppCubit.new);
+  getIt..registerLazySingleton<ApiServices>(() => ApiServices(dio));
 }
