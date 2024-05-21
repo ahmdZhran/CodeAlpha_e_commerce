885eeb7c6f0b6cdc897d77747cd1963dae277c1b
Ahmed <ahmedmohameddx80@gmail.com>
2024-05-21 23:03:17 +0300

    do geneart after create login and user role methods

--- lib/core/graphql/api_services.g.dart
+++ lib/core/graphql/api_services.g.dart
@@ -20,6 +20,62 @@ class _ApiServices implements ApiServices {
 
   String? baseUrl;
 
+  @override
+  Future<LoginRespose> login(Map<String, dynamic> mutation) async {
+    final _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _headers = <String, dynamic>{};
+    final _data = <String, dynamic>{};
+    _data.addAll(mutation);
+    final _result = await _dio
+        .fetch<Map<String, dynamic>>(_setStreamType<LoginRespose>(Options(
+      method: 'POST',
+      headers: _headers,
+      extra: _extra,
+    )
+            .compose(
+              _dio.options,
+              '/graphql',
+              queryParameters: queryParameters,
+              data: _data,
+            )
+            .copyWith(
+                baseUrl: _combineBaseUrls(
+              _dio.options.baseUrl,
+              baseUrl,
+            ))));
+    final value = LoginRespose.fromJson(_result.data!);
+    return value;
+  }
+
+  @override
+  Future<UserRoleResponse> userRole(Map<String, dynamic> getUserRole) async {
+    final _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _headers = <String, dynamic>{};
+    final _data = <String, dynamic>{};
+    _data.addAll(getUserRole);
+    final _result = await _dio
+        .fetch<Map<String, dynamic>>(_setStreamType<UserRoleResponse>(Options(
+      method: 'GET',
+      headers: _headers,
+      extra: _extra,
+    )
+            .compose(
+              _dio.options,
+              'https://api.escuelajs.co/api/v1/auth/profile',
+              queryParameters: queryParameters,
+              data: _data,
+            )
+            .copyWith(
+                baseUrl: _combineBaseUrls(
+              _dio.options.baseUrl,
+              baseUrl,
+            ))));
+    final value = UserRoleResponse.fromJson(_result.data!);
+    return value;
+  }
+
   RequestOptions _setStreamType<T>(RequestOptions requestOptions) {
     if (T != dynamic &&
         !(requestOptions.responseType == ResponseType.bytes ||
