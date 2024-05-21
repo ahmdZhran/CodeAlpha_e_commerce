b8ac1b56e90dc561b7575a8d27a5bf29d40335e4
Ahmed <ahmedmohameddx80@gmail.com>
2024-05-21 22:54:07 +0300

    create first service in api for login response

--- lib/core/graphql/api_services.dart
+++ lib/core/graphql/api_services.dart
@@ -1,11 +1,18 @@
 import 'package:dio/dio.dart';
+import 'package:e_commerce_app/features/auth/data/models/login_response.dart';
 import 'package:retrofit/http.dart';
 
 part 'api_services.g.dart';
 
 const String baseUrl = "https://api.escuelajs.co";
 const String graphql = "/graphql";
+
 @RestApi(baseUrl: baseUrl)
 abstract class ApiServices {
   factory ApiServices(Dio dio, {String baseUrl}) = _ApiServices;
+
+  @POST(graphql)
+  Future<LoginRespose> login(
+    @Body() Map<String, dynamic> mutation,
+  );
 }
